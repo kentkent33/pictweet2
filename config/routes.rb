abf682840627949e79dbbdb180cf28c58a8a5434
EC2 Default User <ec2-user@ip-172-31-80-174.ec2.internal>
2019-08-10 07:44:35 +0000

    image add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- get 'tweets' => 'tweets#index'#2回目"
+ get 'tweets' => 'tweets#index'#3回目"
 end
